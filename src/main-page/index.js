2b1d8e6989acec88d79d466aba61fafd25490260
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-02-19 13:03:02 +0300

    main page

---
+++ src/main-page/index.js
@@ -0,0 +1,13 @@
+import Header from './Header';
+import logo from './logo.svg';
+import './main-page.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
