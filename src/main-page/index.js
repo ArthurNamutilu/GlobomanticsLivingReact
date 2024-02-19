0cc8bbf10f8f7c2fa0220e852dbd9007c8a99c61
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-02-20 00:53:27 +0300

    useMemo()

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
 import Header from './Header';
 import logo from './logo.svg';
 import './main-page.css';
 
+
 function App() {
+
+  const [allHouses, setAllHouses] = useState([]);
+
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, [])
+
   return (
     <div className="container">
-      <Header />
+      <Header subtitle='Providing houses all over Kenya'/>
     </div>
   );
 }
