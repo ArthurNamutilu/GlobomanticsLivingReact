f1507fb17d9f1c8f975d57b4ce0ec60005c91507
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-02-20 00:54:30 +0300

    useMemo()

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import Header from './Header';
 import logo from './logo.svg';
 import './main-page.css';
@@ -15,8 +15,15 @@ function App() {
       setAllHouses(houses);
     };
     fetchHouses();
-  }, [])
+  }, []);
 
+  let featuredHouse = useMemo(() => {
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses]);
+  
   return (
     <div className="container">
       <Header subtitle='Providing houses all over Kenya'/>
