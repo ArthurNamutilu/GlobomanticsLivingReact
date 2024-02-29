aa0036862f370737c894053b4c9cf72e556a3675
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-02-29 23:52:53 +0300

    added searchResult component

---
+++ src/search-results/index.js
@@ -0,0 +1,22 @@
+import SearchResultsRow from "./SearchResultsRow";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+
+const SearchResults = ({ allHouses }) => {
+    const { country } = useParams();
+    const filteredHouse = allHouses.filter((h) => h.country === country)
+
+    return ( 
+        <div className="mt-2">
+            <h4>Results for {country}:</h4>
+            <table className="table table-hover">
+                <tbody>
+                    {filteredHouse.map((h) => (
+                        <SearchResultsRow key={h.id} house={h} />
+                    ))}
+                </tbody>
+            </table>
+        </div>
+     );
+}
+ 
+export default SearchResults;
